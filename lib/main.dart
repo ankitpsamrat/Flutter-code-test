ad1d5ac1da39ca30dbb624004cff963e6b781ceb
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-03 22:03:32 +0530

    call new file

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: ThemeData(
-      //   useMaterial3: true,
-      // ),
       title: 'Code Test',
       // home: HomePage(),
       // home: profile(),
